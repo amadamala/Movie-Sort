b3a4bfbc2ca158e7651edcb0c5209d738c4818ff
Anil <amadamala@gmail.com>
2011-05-21 01:22:25 +0530

    first commit.

---
+++ imdbex.py
@@ -0,0 +1,12 @@
+import imdb
+
+imdb = imdb.IMDb(accessSystem='http')
+movie = imdb.get_movie('0114369')
+
+name = movie['title']
+rating = movie['rating']
+print "Movie name:: %s \nRating:: %0.2f" %(name, rating)
+
+request = self.request()
+item_id = request.uri().split('/')[-1]
+self.data = self.app.get_movie(item_id)
