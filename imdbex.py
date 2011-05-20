b768b526d45aa789280043c6939d63f201a97c03
Anil <amadamala@gmail.com>
2011-05-21 03:18:23 +0530

    printing movie suggestions

--- imdbex.py
+++ imdbex.py
@@ -1,10 +1,13 @@
 # Code to get movie rating from imdb and sort the list of movies.
-from xgoogle.search import GoogleSearch, SearchError
+
 import imdb
 import re
 
+from xgoogle.search import GoogleSearch, SearchError
+from xgoogle.googlesets import GoogleSets
 
-def imdb_url(movie_name):
+
+def get_imdb_url(movie_name):
 	
 	try:
 	# Use xgoogle api to parse google. Following is the url to the api
@@ -15,31 +18,52 @@ def imdb_url(movie_name):
 		results = gs.get_results()
 		url = results[0].url.encode('utf8')
 		# url_title = results[0].title.encode('utf8')
-		# print url, url_title 
-
-		tt = url.split('/')[-2]
-		
-		if(tt[0:2] == 'tt'):
-			movie_id = tt[2:]
-		else:
-			movie_id = None
-			print 'Note: \"%s\"  is\'t listed in imdb as movie name' %movie_name
-		#  print movie name and movie rating using imdbpy api
-		if movie_id != None:
-			im = imdb.IMDb(accessSystem='http')
-			movie = im.get_movie(movie_id)
-			title = movie['title']
-			year = movie['year']
-			rating = movie['rating']
-			print "Movie name:: %s (%d) \nRating:: %0.2f" %(title, year, rating)
+		# print url, url_title
+		return url
 	
 	except SearchError, e:
 		print "Search failed: %s" % e
 
+def imdb_rating(url):
+	tt = url.split('/')[-2]
+	
+	if(tt[0:2] == 'tt'):
+		movie_id = tt[2:]
+	else:
+		movie_id = None
+		print 'Note: \"%s\"  is\'t listed in imdb as movie name' %movie_name
+
+	#  print movie name and movie rating using imdbpy api
+	if movie_id != None:
+		im = imdb.IMDb(accessSystem='http')
+		movie = im.get_movie(movie_id)
+		title = movie['title']
+		year = movie['year']
+		rating = movie['rating']
+		print "Movie name:: %s (%d) \nRating:: %0.2f" %(title, year, rating)
+		print
 
 def main():
-	imdb_url('Godfather')
+	url = get_imdb_url('Godfather')
+	imdb_rating(url)
+	
+	# url = get_imdb_url('Scarface')
+	# imdb_rating(url)
+	# 
+	# url = get_imdb_url('Gladiator')
+	# imdb_rating(url)
 
+    
+	gs = GoogleSets(['Godfather', 'Scarface'])	
+	results = gs.get_results()
+	print 'Some movies you might like: '
+	
+	i = 0
+	while i < 5:
+		print "    ", (i + 1), ".", results[i].encode('utf8')
+		i = i + 1 
+
+				
 if __name__ == '__main__':
 	main()
 	
