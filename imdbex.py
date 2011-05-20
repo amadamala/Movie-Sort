8a48db8c2b166f868f20a8192cd44f82499f7866
Anil <amadamala@gmail.com>
2011-05-21 02:46:07 +0530

    intial version is ready

--- imdbex.py
+++ imdbex.py
@@ -1,13 +1,45 @@
 # Code to get movie rating from imdb and sort the list of movies.
+from xgoogle.search import GoogleSearch, SearchError
 import imdb
+import re
 
-imdb = imdb.IMDb(accessSystem='http')
-movie = imdb.get_movie('0114369')
 
-name = movie['title']
-rating = movie['rating']
-print "Movie name:: %s \nRating:: %0.2f" %(name, rating)
+def imdb_url(movie_name):
+	
+	try:
+	# Use xgoogle api to parse google. Following is the url to the api
+	# http://www.catonmat.net/blog/python-library-for-google-search/
+		search_str = movie_name + ' site:imdb.com'
+		gs = GoogleSearch(search_str)
+		gs.results_per_page = 1
+		results = gs.get_results()
+		url = results[0].url.encode('utf8')
+		# url_title = results[0].title.encode('utf8')
+		# print url, url_title 
 
-request = self.request()
-item_id = request.uri().split('/')[-1]
-self.data = self.app.get_movie(item_id)
+		tt = url.split('/')[-2]
+		
+		if(tt[0:2] == 'tt'):
+			movie_id = tt[2:]
+		else:
+			movie_id = None
+			print 'Note: \"%s\"  is\'t listed in imdb as movie name' %movie_name
+		#  print movie name and movie rating using imdbpy api
+		if movie_id != None:
+			im = imdb.IMDb(accessSystem='http')
+			movie = im.get_movie(movie_id)
+			title = movie['title']
+			year = movie['year']
+			rating = movie['rating']
+			print "Movie name:: %s (%d) \nRating:: %0.2f" %(title, year, rating)
+	
+	except SearchError, e:
+		print "Search failed: %s" % e
+
+
+def main():
+	imdb_url('Godfather')
+
+if __name__ == '__main__':
+	main()
+	
